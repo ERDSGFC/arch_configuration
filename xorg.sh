9b68a88d7ca2bc41b79c735658e384eb30da54b8
qin <3097727187@qq.com>
2024-10-15 18:09:41 +0800

    设置壁纸

--- xorg.sh
+++ xorg.sh
@@ -15,19 +15,23 @@ printMsg(){
 myEcho(){
     echo -e "\033[32m[ ###########${3}########### ]\033[0m"
 }
+# 设置壁纸
+mkdir $HOME/wallpaper
+cp ./.wallpaper.png $HOME/wallpaper/wallpaper.png
+nitrogen --set-zoom-fill $HOME/wallpaper/wallpaper.png
+
 #配置桌面
 mkdir -p $HOME/.config/bspwm $HOME/.config/sxhkd $HOME/.config/alacritty $HOME/.config/polybar
 
 cp ./bspwm/bspwmrc.default $HOME/.config/bspwm/bspwmrc
 cp ./sxhkd/sxhkdrc.default $HOME/.config/sxhkd/sxhkdrc
-
 cp ./polybar/config.default $HOME/.config/polybar/config.ini
 cp ./.xinitrc $HOME/.xinitrc
 cp ./.xprofile $HOME/.xprofile
-#cp /usr/share/doc/bspwm/examples/bspwmrc $HOME/.config/bspwm
-#cp /usr/share/doc/bspwm/examples/sxhkdrc $HOME/.config/sxhkd
-#cp /usr/share/doc/polybar/examples/config.ini $HOME/.config/polybar
-#cp  /etc/X11/xinit/xinitrc $HOME/.xinitrc
+# cp /usr/share/doc/bspwm/examples/bspwmrc $HOME/.config/bspwm
+# cp /usr/share/doc/bspwm/examples/sxhkdrc $HOME/.config/sxhkd
+# cp /usr/share/doc/polybar/examples/config.ini $HOME/.config/polybar
+
 
 cat>$HOME/.config/polybar/launch.sh<<EOF
 #!/bin/bash
@@ -47,4 +51,7 @@ chmod +x $HOME/.config/polybar/launch.sh
 # We use Alacritty's default Linux config directory as our storage location here.
 mkdir -p ~/.config/alacritty/themes
 git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes
-echo IFS='\n' "import = [\n    \"~/.config/alacritty/themes/themes/monokai.toml\"\n]" > ~/.config/alacritty/alacritty.toml
+IFS='\n'
+echo -e "import = [\n    \"~/.config/alacritty/themes/themes/monokai.toml\"\n]" > ~/.config/alacritty/alacritty.toml
+
+
