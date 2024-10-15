45ccb94f36ae5bebb16623be75274eedc225eda6
qin <3097727187@qq.com>
2024-10-15 14:30:52 +0800

    修改alacritty

--- xorg.sh
+++ xorg.sh
@@ -47,4 +47,4 @@ chmod +x $HOME/.config/polybar/launch.sh
 # We use Alacritty's default Linux config directory as our storage location here.
 mkdir -p ~/.config/alacritty/themes
 git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes
-echo "import = [\n    \"~/.config/alacritty/themes/themes/monokai.toml\"\n]" > ~/.config/alacritty/alacritty.toml
+echo IFS='\n' "import = [\n    \"~/.config/alacritty/themes/themes/monokai.toml\"\n]" > ~/.config/alacritty/alacritty.toml
